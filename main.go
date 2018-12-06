bfb76cdc9056a70ce4f9d718a1a8a1c2c0294f78
Thien Nguyen <tatthien.contact@gmail.com>
2018-12-06 14:34:30 +0700

    Support json response

--- main.go
+++ main.go
@@ -1,14 +1,22 @@
 package main
 
 import (
+	"encoding/json"
+
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
 func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	body, _ := json.Marshal(map[string]interface{}{
+		"msg": "Hello from 12bit.vn",
+	})
 	return events.APIGatewayProxyResponse{
+		Headers: map[string]string{
+			"content-type": "application/json",
+		},
 		StatusCode: 200,
-		Body:       "Hello from 12bit.vn",
+		Body:       string(body),
 	}, nil
 }
 
